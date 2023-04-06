5fe593ad262e2e8f3e63d30f04b54b84f42a34d6
이승철 <lud1@naver.com>
2023-04-06 19:53:12 +0900

    프론트 수정

--- act_2023/_layout/index.php
+++ act_2023/_layout/index.php
@@ -1,6 +1,6 @@
 <? include __DIR__.'/../common/db.php'; ?>
 
-<link rel="stylesheet" type="text/css" href="/act_2023/_css/main.css?v=2">
+<link rel="stylesheet" type="text/css" href="/act_2023/_css/main.css?v=<?=time()?>">
 <div id="wrap">
 
 <? include '_layout_top.php'; ?>
@@ -14,14 +14,17 @@
 			<section id="staticBnr">
 				<a class="visual" href="/surfbus"><img src=/act_2023/images/banner/bnrBus.jpg></a>
 			</section>
+			
+			<section id="staticBnr">
+				<a class="visual" href="https://smartstore.naver.com/gustavshof" target="_blank"><img src=/act_2023/images/banner/temp_1680268179503.1017086266.jpeg></a>
+			</section>
 			<section id="promo">
 				<div class="promoInner">
 					<h2>기획전</h2>
 					<div class="promoBox">
-						<a href="https://cafe.naver.com/actrip/381" target="_blank"><img src="/act_2023/images/mainImg/promobg.png" alt=""><span>서핑강습 할인쿠폰 <i class="fas fa-chevron-circle-right"></i><span class="subinst">예약 전에 꼭 할인혜택 체크!</span></span></a>
-						<a href="https://cafe.naver.com/actrip/380" target="_blank"><img src="/act_2023/images/mainImg/promobg.png" alt=""><span>바베큐파티 할인쿠폰 <i class="fas fa-chevron-circle-right"></i><span class="subinst">맛있게 먹고 신나게 즐기자~</span></span></a>
-						<a href="https://cafe.naver.com/actrip/379" target="_blank"><img src="/act_2023/images/mainImg/promobg.png" alt=""><span>서핑버스 할인쿠폰 <i class="fas fa-chevron-circle-right"></i><span class="subinst">비교불가! 빠르고 편리한 액트립 셔틀</span></span></a>
-						<a href="/eatlist"><img src="/act_2023/images/mainImg/promobg.png" alt=""><span>양양 맛도락 여행 <i class="fas fa-chevron-circle-right"></i><span class="subinst">서핑 후엔 맛집에서 체력충전!</span></span></a>
+						<a href="https://cafe.naver.com/actrip/381" target="_blank" style="background: url(../images/mainImg/01.jpg) 0 0/cover no-repeat;"><img src="/act_2023/images/mainImg/promobg.png" alt=""><span>양양 셔틀버스 예약하기 <i class="fas fa-chevron-circle-right"></i><span class="subinst">양양서핑의 불편한 교통은 액트립과 함께!</span></span></a>
+						<a href="https://cafe.naver.com/actrip/380" target="_blank" style="background: url(../images/mainImg/03.jpg) 0 0/cover no-repeat;"><img src="/act_2023/images/mainImg/promobg.png" alt=""><span>동해 셔틀버스 예약하기 <i class="fas fa-chevron-circle-right"></i><span class="subinst">에메랄드빛 동해바다에서 서핑과 힐링을...</span></span></a>
+					
 					</div>
 					<div style="padding-top:20px;"><a href="https://pf.kakao.com/_HxmtMxl" target="_blank"><img src="/act_2023/images/mainImg/kakaochat.jpg" alt="액트립 카카오채널" style="width:100%;"></a></div>
 				</div>
