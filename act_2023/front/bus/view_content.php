19754faa257bde877f80c987495ed33d9924034a
이승철 <lud1@naver.com>
2023-06-25 16:54:00 +0900

    셔틀버스 수정

--- act_2023/front/bus/view_content.php
+++ act_2023/front/bus/view_content.php
@@ -8,17 +8,18 @@
 <?}?>
 
     <?if($param == "surfbus_yy" || $param == "surfbus_yy_frip"){ //양양 셔틀버스?>
-        <img src="https://shop-phinf.pstatic.net/20230216_224/16765144423533g2Ic_JPEG/2023_%EB%B2%84%EC%8A%A4%EC%83%81%EC%84%B8_2.jpg?type=w860" class="placeholder">
-        <img src="https://shop-phinf.pstatic.net/20230216_61/1676514452486wp34G_JPEG/2023_%EB%B2%84%EC%8A%A4%EC%83%81%EC%84%B8_3.jpg?type=w860" class="placeholder">
-        <img src="https://shop-phinf.pstatic.net/20230216_152/1676537938201jQrgn_JPEG/2023_%EB%B2%84%EC%8A%A4%EC%83%81%EC%84%B8_%EC%84%9C%EC%9A%B8%ED%96%89(%EC%96%91%EC%96%91)_2.jpg?type=w860" class="placeholder">
+        <img src="https://shop-phinf.pstatic.net/20230625_103/1687658082342xrqAG_JPEG/1.jpg?type=w860" class="placeholder">
+        <img src="https://shop-phinf.pstatic.net/20230625_142/1687657412810ide68_JPEG/2.jpg?type=w860" class="placeholder">
+        <img src="https://shop-phinf.pstatic.net/20230625_86/16876574238213NXk9_JPEG/3.jpg?type=w860" class="placeholder">
+        <img src="https://shop-phinf.pstatic.net/20230625_170/1687657455464ydvBD_JPEG/4.jpg?type=w860" class="placeholder">
         
         <?if($arrChannel == ""){?>
-        <img src="https://shop-phinf.pstatic.net/20230216_168/16765144636841QA9a_JPEG/2023_%EB%B2%84%EC%8A%A4%EC%83%81%EC%84%B8_4_2.jpg?type=w860" class="placeholder">
+        <img src="https://shop-phinf.pstatic.net/20230228_140/1677547409989O2Qw3_JPEG/2023_%EB%B2%84%EC%8A%A4%EC%83%81%EC%84%B8_4_%EC%88%98%EC%A0%95.jpg?type=w860" class="placeholder">
         <?}?>
         
-        <img src="https://shop-phinf.pstatic.net/20230424_83/1682301226431RFaR7_JPEG/%EC%96%91%EC%96%91%ED%96%89_%EC%82%AC%EB%8B%B9%EC%84%A0.jpg?type=w860" class="placeholder">
-        <img src="https://shop-phinf.pstatic.net/20230424_101/1682301231763AGHoC_JPEG/%EC%96%91%EC%96%91%ED%96%89_%EC%A2%85%EB%A1%9C%EC%84%A0.jpg?type=w860" class="placeholder">
-        <img src="https://shop-phinf.pstatic.net/20230216_177/1676514508542hxrVx_JPEG/%EC%84%9C%EC%9A%B8%ED%96%89_%EC%96%91%EC%96%91.jpg?type=w860" class="placeholder">
+        <img src="https://shop-phinf.pstatic.net/20230625_108/1687657587809nmsc6_JPEG/%EC%82%AC%EB%8B%B9%EC%84%A0.jpg?type=w860" class="placeholder">
+        <img src="https://shop-phinf.pstatic.net/20230625_249/1687657605744HDSdY_JPEG/%EC%A2%85%EB%A1%9C%EC%84%A0.jpg?type=w860" class="placeholder">
+        <img src="https://shop-phinf.pstatic.net/20230625_61/1687657614128OxVse_JPEG/%EC%84%9C%EC%9A%B8%ED%96%89_%EC%96%91%EC%96%91.jpg?type=w860" class="placeholder">
     <?}else if($param == "surfbus_dh"){ //동해 셔틀버스?>
         <img src="https://shop-phinf.pstatic.net/20230216_199/1676537114198JevGo_JPEG/2023_%EB%B2%84%EC%8A%A4%EC%83%81%EC%84%B8_2.jpg?type=w860" class="placeholder">
         <img src="https://shop-phinf.pstatic.net/20230216_130/1676537121294WBWK7_JPEG/2023_%EB%B2%84%EC%8A%A4%EC%83%81%EC%84%B8_3.jpg?type=w860" class="placeholder">
