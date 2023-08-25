74877807a9a5504483444584572e7e11040b6251
이승철 <lud1@naver.com>
2023-08-25 12:03:21 +0900

    동해 셔틀버스 작업

--- act_2023/common/func.php
+++ act_2023/common/func.php
@@ -536,4 +536,38 @@ function fnBusCode($code, $bustype){
 		return $rtnCode2.$code2;
 	}
 }
+
+//네이버 단축URL 생성
+function shortURL($url){
+	$client_id = "zhh3svia3i";
+	$client_secret = "HndcoWtPGYL7hw9TVvhviZzEL7Sg921WUEIDdWxw";
+  
+	$encText = urlencode($url);
+	$postvars = "url=".$encText;
+  
+	$url = "https://naveropenapi.apigw.ntruss.com/util/v1/shorturl";
+  
+	$is_post = true;
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_POST, $is_post);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch,CURLOPT_POSTFIELDS, $postvars);
+  
+	$headers = array();
+	$headers[] = "X-NCP-APIGW-API-KEY-ID: ".$client_id;
+	$headers[] = "X-NCP-APIGW-API-KEY: ".$client_secret;
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  
+	$response = curl_exec ($ch);
+	$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  
+	//echo "status_code:".$status_code."<br />";
+	curl_close ($ch);
+	if($status_code == 200) {
+	  return $response;
+	} else {
+	  return "Error :".$response;
+	}
+}
 ?>
